b8a7493b3246fc9f2826997f3fdb532869d39504
Iris1902 <irispicis1962@gmail.com>
2025-06-22 23:55:01 -0500

    :wrench: fix(main.tf): Fix health check path in AWS LB target group configuration

--- modules/microservice/main.tf
+++ modules/microservice/main.tf
@@ -54,7 +54,7 @@ resource "aws_lb_target_group" "tg" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   health_check {
-    path                = "/healthz"
+    path                = "/health"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 3
