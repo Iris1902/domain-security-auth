bb0da12c11f247a45cdc8c9831f6a372a4413967
Iris1902 <irispicis1962@gmail.com>
2025-06-23 14:47:36 -0500

    :cleanup: remove commented-out resources for S3 and DynamoDB in main.tf

--- main.tf
+++ main.tf
@@ -15,23 +15,6 @@ provider "aws" {
   token      = var.AWS_SESSION_TOKEN
 }
 
-# --- Recursos para crear S3 y DynamoDB para el backend remoto (SOLO USAR SI NECESITAS CREARLOS DE NUEVO) ---
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket        = "domain-security-auth-tfstate"
-#   force_destroy = true
-# }
-
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "domain-security-auth-tfstate-locks"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
-
 # Módulos para cada microservicio que desees desplegar
 module "encrypt" {
   source       = "./modules/microservice"
